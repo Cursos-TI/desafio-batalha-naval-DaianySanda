4a5d5a7bfbf4fd3a649f6ad75d1bbc8012bfbabe
DaianySanda <daianydomingos@hotmail.com>
2025-04-24 01:12:39 +0000

    nivel mestre, inclusão do cone, cruz e octaedro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 //1. Declara variáveis
-int linhas = 10;
-int colunas = 10;
+#define linhas 10
+#define colunas 10
 
 
 int main (){
@@ -25,14 +25,16 @@ int main (){
                 printf("%d ", tabuleiro[i][j] =  0);
             }
             printf("\n");
-        }
+        } 
 
         
     //4. Coordenadas e tamanho
     int navio_horizontal_coluna = 1;
-    int navio_horizontal_linha = 6;
+    int navio_horizontal_linha = 8;
     int navio_vertical_coluna = 8;
     int navio_vertical_linha = 5;
+    int navio_diagonal_linha = 2;
+    int navio_diagonal_coluna = 1;
     int tamanho_navio = 3;
 
     //5.Posicionar os navios (Usar loop's para posicionar o navio)
@@ -61,9 +63,41 @@ int main (){
         } 
     }
 
+    //5.3 Navio diagonal 1 esquerda para a direita
+
+    int navio_diagonal1_linha = 2;
+    int navio_diagonal1_coluna = 2;
+    int tamanho_navio_diagonal = 3;
+
+
+    if (navio_diagonal1_linha + tamanho_navio_diagonal > linhas || 
+    navio_diagonal1_coluna + tamanho_navio_diagonal > colunas) {
+    printf("Opção inválida para navio diagonal!\n");
+    } else {
+    for (int i = 0; i < tamanho_navio_diagonal; i++) {
+        tabuleiro[navio_diagonal1_linha + i][navio_diagonal1_coluna + i] = 3;
+    }
+    }
+    
+    
+    //5.4 Navio diagonal 2 direita para a esquerda 
+    int navio_diagonal2_linha = 2;
+    int navio_diagonal2_coluna = 6;
+    int tamanho_navio_diagonal2 = 3;
+
+  
+    if (navio_diagonal2_linha + tamanho_navio_diagonal2 > linhas ||
+    navio_diagonal2_coluna - (tamanho_navio_diagonal2 - 1) < 0) {
+    printf("Opção inválida para navio diagonal 2!\n");
+    } else {
+    for (int i = 0; i < tamanho_navio_diagonal2; i++) {
+        tabuleiro[navio_diagonal2_linha + i][navio_diagonal2_coluna - i] = 3;
+    }
+    }
+
     //6.Imprimir novamente o tabuleiro
     printf("\n");
-    printf("Primeira Rodada!\n");
+    printf("Navios posicionados!\n");
     printf("    %c %c %c %c %c %c %c %c %c %c\n", linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[8], linha[9]);
 
     for (int i = 0; i < linhas; i++)
